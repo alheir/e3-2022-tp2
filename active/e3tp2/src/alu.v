c57a4d82b7a3a39dc75e1abd1708daa602fb64f1
Ale Heir <aheir@itba.edu.ar>
2022-10-30 22:02:48 -0300

    activehdl empty files

---
+++ active/e3tp2/src/alu.v
@@ -0,0 +1,31 @@
+//-----------------------------------------------------------------------------
+//
+// Title       : alu
+// Design      : calc
+// Author      : aheir
+// Company     : aheir
+//
+//-----------------------------------------------------------------------------
+//
+// File        : F:\documents\alheir\e3-2022-tp2\active\e3tp2\src\alu.v
+// Generated   : Sun Oct 30 22:00:53 2022
+// From        : interface description file
+// By          : Itf2Vhdl ver. 1.22
+//
+//-----------------------------------------------------------------------------
+//
+// Description : 
+//
+//-----------------------------------------------------------------------------
+`timescale 1 ns / 1 ps
+
+//{{ Section below this comment is automatically maintained
+//   and may be overwritten
+//{module {alu}}
+module alu ();
+
+//}} End of automatically maintained section
+
+// -- Enter your statements here -- //
+
+endmodule
---
+++ active/e3tp2/src/keyboard.v
@@ -0,0 +1,31 @@
+//-----------------------------------------------------------------------------
+//
+// Title       : keyboard
+// Design      : calc
+// Author      : aheir
+// Company     : aheir
+//
+//-----------------------------------------------------------------------------
+//
+// File        : F:\documents\alheir\e3-2022-tp2\active\e3tp2\src\keyboard.v
+// Generated   : Sun Oct 30 22:02:09 2022
+// From        : interface description file
+// By          : Itf2Vhdl ver. 1.22
+//
+//-----------------------------------------------------------------------------
+//
+// Description : 
+//
+//-----------------------------------------------------------------------------
+`timescale 1 ns / 1 ps
+
+//{{ Section below this comment is automatically maintained
+//   and may be overwritten
+//{module {keyboard}}
+module keyboard ();
+
+//}} End of automatically maintained section
+
+// -- Enter your statements here -- //
+
+endmodule
